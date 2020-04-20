afdd3e96b84f47ef39bab671c5e40b8f4ef38893
rickutino <rickutino@gmail.com>
2020-04-21 04:48:44 +0900

    backend
    create Controller, Model, Jbuider File and connect to the EndPoints application.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: 'home#index'
+
+  concern :favoritable do |options|
+    shallow do
+      post "/favorite", { to: "favorites#create", on: :member }.merge(options)
+      delete "/favorite", { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+      resources :search, only: :index
+      resources :categories, only: [:index,:show]
+      resources :albums, only: :show do
+        resources :recently_heards, only: :create
+      end
+      resources :favorites, only: :index
+      
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
+      end  
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

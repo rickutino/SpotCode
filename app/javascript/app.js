ff75ba45fc5b9321c1a95ee60b919743f8245bb9
rickutino <rickutino@gmail.com>
2020-04-26 22:54:22 +0900

    Including React in the Project

---
+++ app/javascript/app.js
@@ -0,0 +1,10 @@
+import React,{ Fragment } from 'react';
+
+    const App = () =>{
+      return(
+        <Fragment>
+          Hello App
+        </Fragment>
+      );
+    }
+    export default App;

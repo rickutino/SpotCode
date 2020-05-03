7c802b840cf2d5afdb7ec8b17ce1728052f51e4d
rickutino <rickutino@gmail.com>
2020-05-03 17:11:00 +0900

    add Botton enter to Login

--- app/javascript/components/common/menu/index.js
+++ app/javascript/components/common/menu/index.js
@@ -1,20 +1,39 @@
 import React, { Fragment } from 'react';
-import { Navbar, Container, Columns } from 'react-bulma-components';
+import { Navbar, Container, Columns, Button } from 'react-bulma-components';
 import ImgLogo from '../../../assets/images/logo.png';
+import Styled from 'styled-components';
+import { useLocation } from 'react-router-dom';
+
+const ColumnsFullWidth = Styled(Columns)`
+  width: 100%;
+  padding-top: 20px;
+  padding-left: 10px;
+  padding-bomton: 10px;
+`
 
 const Menu = () => {
+  let actionBotton;
+
+  if(useLocation().pathname == '/'){
+    actionBotton =<a href='/users/sign_in' className="is-pulled-right is-right">
+      <Button outlined={true} color="white">ENTER</Button>
+    </a>
+  } else {
+
+  }
+
   return (
   <Fragment>
     <Navbar color="dark">
       <Container>
-        <Columns className="is-mobile">
+        <ColumnsFullWidth className="is-mobile">
           <Columns.Column desktop={{size: 2}} mobile={{size: 5}}>
-            <img src={ImgLogo}/>
+            <img src={ImgLogo} className="image"/>
           </Columns.Column>
           <Columns.Column>
-
+            {actionBotton}
           </Columns.Column>
-        </Columns>
+        </ColumnsFullWidth>
       </Container>
     </Navbar>
   </Fragment>

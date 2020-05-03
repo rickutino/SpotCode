a3b5b4f7a75fbd1ae90c00b4189ec4f772bfe014
rickutino <rickutino@gmail.com>
2020-05-03 15:53:01 +0900

    Revert "test the components"
    This reverts commit 1253949f41d769b91e17a42964a68a8d3a064e45.

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,15 +1,15 @@
 import  React, { Fragment } from 'react';
-import 'react-bulma-components/dist/react-bulma-components.min.css'; 
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components';
 import 'app.scss';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
-import Menu from './components/common/menu';
 
 const App = () =>{
   return(
     <Fragment>
+      <Button color="primary">Test the Bulma</Button>
       <BrowserRouter>
-        <Menu/>
         <Routes/>
       </BrowserRouter>
     </Fragment>

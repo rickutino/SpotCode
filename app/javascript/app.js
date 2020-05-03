a86f92b5ca4e9f7ef0b44d602834a533e71c2925
rickutino <rickutino@gmail.com>
2020-05-03 12:20:24 +0900

    implementation and test of BrowserRoute

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -2,12 +2,17 @@ import  React, { Fragment } from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import { Button } from 'react-bulma-components';
 import 'app.scss';
+import Routes BrowserRoutefrom './routes';
+import { r } from 'react-router-dom';
 
-    const App = () =>{
-      return(
-        <Fragment>
-          <Button color="primary">Test the Bulma</Button>
-        </Fragment>
-      );
-    }
-    export default App;
+const App = () =>{
+  return(
+    <Fragment>
+      <Button color="primary">Test the Bulma</Button>
+      <BrowserRouter>
+        <Routes/>
+      </BrowserRouter>
+    </Fragment>
+  );
+}
+export default App;
---
+++ app/javascript/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeScreen from './screens/home';
+import { Switch, Route } from 'react-router-dom'; 
+
+const Routes = () => (
+  <Switch>
+    <Route exact path='/' component={HomeScreen}/>
+  </Switch>
+)
+export default Routes;

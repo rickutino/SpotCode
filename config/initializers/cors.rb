b231bad1b0ab282a3db412f39f09481137d8c9ad
rickutino <rickutino@gmail.com>
2020-05-10 00:17:52 +0900

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
   origins '*' #<= Colocar o endereco url da pagina
               # Para que so a pagina autorizada possa acessar os campos a baixo.
-​
+
   resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
